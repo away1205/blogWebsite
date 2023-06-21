02c7a0af09718bda8d394ed3d2c8b54cbb3fe60f
Away <agwamayatullah@gmail.com>
2023-06-21 13:22:14 +0700

    First Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.send('test')
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Running in 3000')
+})
+
